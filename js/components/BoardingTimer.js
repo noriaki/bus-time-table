32612aa3f86e5da5c469b205388d3a79ec360428
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-03-28 17:48:53 +0900

    add timer displaying component

--- js/components/BoardingTimer.js
+++ js/components/BoardingTimer.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import moment from 'moment';
 import { StyleSheet, View, Text } from 'react-native';
 
-const BoardingTimer = ({ remaining }) => (
-  <View><Text>{remaining}</Text></View>
-);
+const BoardingTimer = ({ remaining }) => {
+  const m = moment(remaining);
+  const haveHour = m.valueOf() >= 60 * 60 * 1000;
+  return (
+    <View style={styles.container}>
+      <Text>次のバス発車まで</Text>
+      {haveHour ? <Hour m={m} /> : null}
+      {haveHour ? <Suffix str="時間" /> : null}
+      <Minute m={m} /><Suffix str="分" />
+      <Second m={m} /><Suffix str="秒" />
+    </View>
+  );
+};
+
+const Hour = ({ m }) => <Text style={styles.timer}>{m.format('HH')}</Text>;
+const Minute = ({ m }) => <Text style={styles.timer}>{m.format('mm')}</Text>;
+const Second = ({ m }) => <Text style={styles.timer}>{m.format('ss')}</Text>;
+const Suffix = ({ str }) => <Text style={styles.suffix}>{str}</Text>;
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    alignItems: 'baseline',
+    justifyContent: 'center',
+    margin: 10,
+  },
+  timer: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginLeft: 5,
+  },
+  suffix: {
+    fontSize: 10,
+  },
 });
 
 export default BoardingTimer;
